10f984cecb9c83c9c7086899c50ec54a8f848a4d
Alvinnzz <alvintripamukas@gmail.com>
2023-10-08 21:57:37 +0700

    add file

---
+++ alfin_tri_pamungkas.py
@@ -0,0 +1,16 @@
+
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+penjumlahan = bilangan1 + bilangan2
+pengurangan = bilangan1 - bilangan2
+perkalian = bilangan1 * bilangan2
+pembagian = bilangan1 / bilangan2
+
+pembulatan_pembagian = round(pembagian)
+
+print("Hasil penjumlahan: ", penjumlahan)
+print("Hasil pengurangan: ", pengurangan)
+print("Hasil perkalian: ", perkalian)
+print("Hasil pembagian: ", pembagian)
+print("Pembulatan pembagian: ", pembulatan_pembagian)
